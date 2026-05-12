b6c03501640d069eafc96160f98992209333b19f
Tester <test@example.com>
2026-05-12 23:16:44 +0600

    Fix: Add gradle properties and dummy java activity

---
+++ app/src/main/java/com/falconeye/rat/MainActivity.java
@@ -0,0 +1,11 @@
+package com.falconeye.rat;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
